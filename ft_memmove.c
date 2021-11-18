3df5e49305521bb2dae8bcfdda38881022a69d0a
sooyeon lee <2sy0127@gmail.com>
2021-11-19 00:28:44 +0900

    libft

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sooyeon <sooylee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 18:35:51 by sooyeon           #+#    #+#             */
+/*   Updated: 2021/11/18 18:36:18 by sooyeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t num)
 {
-        unsigned char   *new_dst;
-        unsigned char   *new_src;
-        size_t                  i;
+	unsigned char	*new_dst;
+	unsigned char	*new_src;
+	size_t			i;
 
-        if (!dst && !src)
-                return (0);
-        new_dst = (unsigned char *) dst;
-        new_src = (unsigned char *)src;
-        i = 0;
-        while (i < num)
+	if (!dst && !src)
+		return (0);
+	new_dst = (unsigned char *) dst;
+	new_src = (unsigned char *)src;
+	i = 0;
+	while (i < num)
 	{
-                new_dst[i] = new_src++[i];
-                i++;
+		new_dst[i] = new_src++[i];
+		i++;
 	}
-        return (dst);
+	return (dst);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,23 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sooyeon <sooylee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/18 18:37:49 by sooyeon           #+#    #+#             */
-/*   Updated: 2021/11/18 18:37:51 by sooyeon          ###   ########.fr       */
+/*   Created: 2021/11/18 18:34:00 by sooyeon           #+#    #+#             */
+/*   Updated: 2021/11/18 18:34:03 by sooyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlcpy(char *dst, const char *src, size_t len)
 {
-	int	i;
+	size_t			i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (src[i] && i + 1 < len)
+	{
+		dst[i] = src[i];
 		i++;
-	return (i);
+	}
+	dst[i] = '\0';
+	return (ft_strlen(scr));
 }
