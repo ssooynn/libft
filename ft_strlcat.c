714fdfde1440f24cdacb2a68f3924cb2544ffdf2
Sooyeon Lee <sooylee@student.42seoul.kr>
2021-11-19 15:48:20 +0900

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,6 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sooylee <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 15:45:22 by sooylee           #+#    #+#             */
+/*   Updated: 2021/11/19 15:47:08 by sooylee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t len)
 {
-	
+	size_t	dst_len;
+	size_t	src_len;
+
+	if (!dst || !src)
+		return (0);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (len < dst_len)
+		return (src_len + len);
+	dst = dst + dst_len;
+	ft_strlcpy(dst, src, n - dst_len);
+	return (src_len + dst_len);
 }
