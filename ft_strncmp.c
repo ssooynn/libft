5d893ed9a738bbf1033c08b60d3b37f328e620cd
sooyeon lee <2sy0127@gmail.com>
2021-12-01 23:01:37 +0900

    libft

---
+++ ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sooyeon <sooylee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 23:01:20 by sooyeon           #+#    #+#             */
+/*   Updated: 2021/12/01 23:01:21 by sooyeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+int	ft_strncmp(const char *s1, const char *s2, size_t len)
+{
+	size_t		i;
+	unsigned char	*b1;
+	unsigned char	*b2;
+
+	b1 = (unsigned char *)s1;
+	b2 = (unsigned char *)s2;
+	i = 0;
+	while (i < len)
+	{
+		if(b1[i] != b2[i] || b1[i] == 0 || b2[i] == 0)
+			return (b1[i] - b2[i]);
+		i++;
+	}
+	return (0);
+}
